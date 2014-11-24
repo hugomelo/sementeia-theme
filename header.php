d69d8f48a1839b6e717b2fd1c76a679b8431d501
Hugo Melo <hugo@riseup.net>
2014-11-24 12:23:01 -0200

    Add clicky code

--- header.php
+++ header.php
@@ -6,6 +6,19 @@
 	<?php wp_head();?>
 	<script type="text/javascript" src="<?php bloginfo('template_url')?>/js/sementeia.js"></script>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<script type="text/javascript">
+var clicky_site_ids = clicky_site_ids || [];
+clicky_site_ids.push(100791911);
+(function() {
+	var s = document.createElement('script');
+	s.type = 'text/javascript';
+	s.async = true;
+	s.src = '//static.getclicky.com/js';
+	( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+	 })();
+</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100791911ns.gif" /></p></noscript> )
+})
 </head>
 
 <?php
